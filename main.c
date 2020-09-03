711bf730a094600894ec70f66ad887acae6a1f76
YuHsiang Huang <ykh5222@psu.edu>
2020-09-03 15:15:07 +0000

    first try of lab1c

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
+// author Yu-Hsiang, Huang ykh5222@psu.edu
+// collaborator Nidhi Swammy nms6241@psu.edu
+// collaborator Yiquan Li yvl5838@psu.edu
+// collaborator 
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 int main(void) {
-  char* cstr = readline("Enter temperature in Celsius");
-  double celsius = atof(cstr);
+  char *cstr = readline("Enter temperature in celsius: ");
+  double ctemp = atof(cstr);
+  double fstr = atof(ctemp * 9/5 + 32);
+  printf("%s° in celsius is equivalent to %f° fahrenheit.\n", cstr, fstr);
   return 0;
 }
